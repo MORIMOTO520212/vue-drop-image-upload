9fec75cc477b03d9d7dbcd5cec78ee1d99d43a21
root <root@XSUSA>
2023-08-03 23:52:11 +0900

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# vue-drop-image-upload - Vue 3 in Vite
+
+
+
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Run Unit Tests with [Vitest](https://vitest.dev/)
+
+```sh
+npm run test:unit
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const dragState = ref("null");
+const isEnter = ref();
+
+const handleDragEnter = (e: Event) => {
+  dragState.value = "handleDragEnter";
+  isEnter.value = true;
+  e.preventDefault();
+}
+
+const handleDragLeave = (e: Event) => {
+  dragState.value = "handleDragLeave";
+  isEnter.value = false;
+  e.preventDefault();
+}
+
+const handleDrop = (e) => {
+  e.preventDefault();
+}
+</script>
+
+<template>
+  <form action="#" method="post">
+    <div 
+      :class="`drop-area ${ isEnter ? 'isEnter' : '' }`"
+      @dragenter="handleDragEnter"
+      @dragleave="handleDragLeave"
+      @drop="handleDrop"
+      @dragover.prevent
+    >
+      <p>Image file drop here.</p>
+      <p>{{ dragState }}</p>
+    </div>
+    <input type="file" hidden />
+  </form>
+</template>
